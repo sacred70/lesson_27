248b64d248b7e12bdb9018e65748056c816ac333
sacred70 <sacred70@mail.ru>
2023-05-01 23:03:17 +0700

    1

--- ads/views.py
+++ ads/views.py
@@ -8,8 +8,10 @@ from django.views.generic import DetailView
 
 from ads.models import Category, Ad
 
+
 def root(request):
     return JsonResponse({"status": "ok"}, status=200)
+
 
 @method_decorator(csrf_exempt, name="dispatch")
 class CategoryListCreateView(View):
@@ -28,19 +30,22 @@ class AdListCreateView(View):
     def get(self, request):
         all_ads = Ad.object.all
         return JsonResponse([ad.serialize() for ad in all_ads], safe=False)
+
     def post(self, request):
         data = json.loads(request.body)
         new_ad = Ad.objects.create(**data)
         return JsonResponse(new_ad.serialize())
 
+
 class AdDitailView(DetailView):
     model = Ad
 
     def get(self, request, *args, **kwargs):
-        return self.get_object().serialize()
+        return JsonResponse(self.get_object().serialize())
+
 
 class CategoryDitailView(DetailView):
     model = Category
 
     def get(self, request, *args, **kwargs):
-        return self.get_object().serialize()
+        return JsonResponse(self.get_object().serialize())
